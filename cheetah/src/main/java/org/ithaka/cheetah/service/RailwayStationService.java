86ad9e1b57e4dabe00c25ba0d95d76af9373d14d
tom green <thomas.green@ithaka.org>
2014-08-16 23:14:40 -0400

    refresh project

---
+++ cheetah/src/main/java/org/ithaka/cheetah/service/RailwayStationService.java
@@ -0,0 +1,21 @@
+package org.ithaka.cheetah.service;
+
+import java.util.List;
+
+import org.ithaka.cheetah.beans.RailwayStation;
+
+
+public interface RailwayStationService {
+	
+    public List<RailwayStation> getAllRailwayStations();
+
+    public RailwayStation getRailwayStationById(Long id);
+
+    public void addRailwayStation(RailwayStation RailwayStation);
+
+    public void deleteRailwayStationById(Long id);
+
+    public void deleteAll();
+
+    public void updateRailwayStation(RailwayStation RailwayStation);
+}
---
+++ cheetah/src/main/java/org/ithaka/cheetah/service/TrainService.java
@@ -0,0 +1,19 @@
+package org.ithaka.cheetah.service;
+
+import java.util.List;
+
+import org.ithaka.cheetah.beans.Train;
+
+public interface TrainService {
+    public List<Train> getAllTrains();
+
+    public Train getTrainById(Long id);
+
+    public void addTrain(Train train);
+
+    public void deleteTrainById(Long id);
+
+    public void deleteAll();
+
+    public void updateTrain(Train train);
+}
